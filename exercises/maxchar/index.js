7723a62dbfb01bd51c37352ab835d9dc1c159b6d
rochellev <rochellevroberts@utexas.edu>
2021-03-22 16:12:23 -0700

    add my solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// convert to obj
+// keys: the character, value: the count
+
+// (1) my solution: make obj with keys as chars and their counts as val
+// while generating object, do checks for current max then return that max char
+function maxChar(str) {
+  let maxCount = 0;
+  let maxChar = "";
+  let chars = {};
+  str.split("").forEach(element => {
+    chars[element] = chars[element] + 1 || 1;
+    if (chars[element] > maxCount) {
+      maxCount = chars[element];
+      maxChar = element;
+    }
+  });
+
+  return maxChar;
+}
 
 module.exports = maxChar;
