5a93782ec0b44cb97a52322c504a7622211d11fb
rochellev <rochellevroberts@utexas.edu>
2021-03-22 10:44:41 -0700

    solution using reverse prototype

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,16 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// function reverse(str) {
+//   let result = [];
+//   if (str.length === 1) {
+//     return str;
+//   }
+//   // console.log(str);
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     result.push(str[i]);
+//   }
+//   return result.join("");
+// }
+
+// use built in array reverse prototype
+// spit into array
+// then reverse and join with empty string
+// otherwise default is comma
 function reverse(str) {
-  let result = [];
-  if (str.length === 1) {
-    return str;
-  }
-  // console.log(str);
-  for (let i = str.length - 1; i >= 0; i--) {
-    result.push(str[i]);
-  }
-  return result.join("");
+  // const arr = str.split("");
+  // return arr.reverse().join("");
+  return str
+    .split("")
+    .reverse()
+    .join("");
 }
 
 module.exports = reverse;
