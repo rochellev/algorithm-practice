450a5260ac21a1293955aa7d3464d1e380e86cc2
rochellev <rochellevroberts@utexas.edu>
2021-03-22 15:01:44 -0700

    finished solution to reverse int, the trick was converting to string then array to reversethen using parseInt to get it back to a number along with multiplying it by Math.sign to get the right sign going

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// (1) My solution - had a hard time
+function reverseInt(n) {
+  let rev = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+  return parseInt(rev) * Math.sign(n);
+}
 
 module.exports = reverseInt;
