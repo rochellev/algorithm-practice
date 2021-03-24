3052f655f98ef92acd57dba7c1494ff321286a5e
rochellev <rochellevroberts@utexas.edu>
2021-03-24 16:45:45 -0700

    add other solutions to vowel problem

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,43 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// (0) convert to array to use filter then take that length
+// function vowels(str) {
+//   return str
+//     .toLowerCase()
+//     .split("")
+//     .filter(curr => {
+// if (
+//   curr == "a" ||
+//   curr == "e" ||
+//   curr == "i" ||
+//   curr == "o" ||
+//   curr == "u"
+// ) {
+//         return curr;
+//       }
+//     }).length;
+// }
 
+// (1) iterative solution
+// make use of includes helper method
+// function vowels(str) {
+//   let count = 0;
+//   const vowelString = ["a", "e", "i", "o", "u"];
+//   for (let curr of str.toLowerCase()) {
+//     if (vowelString.includes(curr)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// (2) RegEx solution
+// [] - if contains anything in square brackets, let us know
+// g  - look at whole string, don't stop at beginning
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  // null falsey -- returned if no matches
+  return matches ? matches.length : 0;
+}
 module.exports = vowels;
