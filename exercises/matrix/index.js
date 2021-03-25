b7f9ca0a217268ff424eaf5b9fa9efe59c3c1461
rochellev <rochellevroberts@utexas.edu>
2021-03-25 12:22:22 -0700

    finish matrix, tougher problem

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,52 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// solution:
+// have boundaries. for loop for each direction
+// to right --> down --> to left ---> up ---> repeat
+
+function matrix(n) {
+  // construct empty n by n matrix array
+  let result = Array(n)
+    .fill(null)
+    .map(() => Array(n).fill(null));
+  // counter is the num to be inserted
+  let counter = 1;
+  // init boundaries
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  // outer loop: go though each row/col
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // loop to fill segment from start to end column, [startRow][currColumn]
+    // the horizontal move
+    for (let i = startColumn; i <= endColumn; i++) {
+      result[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+    // now the vertical down (increasing row)
+    for (let r = startRow; r <= endRow; r++) {
+      result[r][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+    // now to left [endRow][currCol]
+    for (let currColumn = endColumn; currColumn >= startColumn; currColumn--) {
+      result[endRow][currColumn] = counter;
+      counter++;
+    }
+    endRow--;
+    // up again [currRow][startColumn]
+    for (let cr = endRow; cr >= startRow; cr--) {
+      result[cr][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+  return result;
+}
 
 module.exports = matrix;
