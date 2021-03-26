8dcc17973339643afe1fc5a761246c7fa7b21bfd
rochellev <rochellevroberts@utexas.edu>
2021-03-26 12:22:49 -0700

    add peek method to queue class'

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -3,7 +3,7 @@
 // 2) Implement the 'weave' function.  Weave
 // receives two queues as arguments and combines the
 // contents of each into a new, third queue.
-// The third queue should contain the *alterating* content
+// The third queue should contain the *alternating* content
 // of the two queues.  The function should handle
 // queues of different lengths without inserting
 // 'undefined' into the new one.
@@ -24,6 +24,9 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+  let combinedQ = new Queue();
+  
+}
 
 module.exports = weave;
