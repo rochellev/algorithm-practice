ee946d4a2013b4ba43c1e6b0d0f3664176c87a68
rochellev <rochellevroberts@utexas.edu>
2021-03-26 16:06:07 -0700

    add two solutions for implementing a queue from two stacks

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -1,5 +1,5 @@
 // --- Directions
-// Implement a Queue datastructure using two stacks.
+// Implement a Queue data structure using two stacks.
 // *Do not* create an array inside of the 'Queue' class.
 // Queue should implement the methods 'add', 'remove', and 'peek'.
 // For a reminder on what each method does, look back
@@ -12,8 +12,66 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+// another way is to do work for remove and peek
+// i prefer my method because only add does the heavy logic
+// this solution has repeated logic
+class Queue {
+  constructor() {
+    this.stackA = new Stack();
+    this.stackB = new Stack();
+  }
+  add(record) {
+    this.stackA.push(record);
+  }
+  remove() {
+    while (this.stackA.peek()) {
+      this.stackB.push(this.stackA.pop());
+    }
+    const firstRecord = this.stackB.pop();
+    while (this.stackB.peek()) {
+      this.stackA.push(this.stackB.pop());
+    }
+    return firstRecord;
+  }
+  peek() {
+    while (this.stackA.peek()) {
+      this.stackB.push(this.stackA.pop());
+    }
+    const firstRecord = this.stackB.peek();
+    while (this.stackB.peek()) {
+      this.stackA.push(this.stackB.pop());
+    }
+    return firstRecord;
+  }
+}
+
+// my way does the ordering work in add method
+
+// class Queue {
+//   constructor() {
+//     this.stackA = new Stack();
+//     this.stackB = new Stack();
+//   }
+//   add(record) {
+//     // pop all records out of stackA
+//     while (this.stackA.peek()) {
+//       this.stackB.push(this.stackA.pop());
+//     }
+//     // add in new one to "last" position
+//     this.stackA.push(record);
+//     // re-pop everything from b to a
+//     while (this.stackB.peek()) {
+//       this.stackA.push(this.stackB.pop());
+//     }
+//   }
+//   remove() {
+//     return this.stackA.pop();
+//   }
+//   peek() {
+//     return this.stackA.peek();
+//   }
+// }
 
 module.exports = Queue;
