f810fbcbc52516015b66ceeeb864e15cff1f118a
rochellev <rochellevroberts@utexas.edu>
2021-03-31 15:49:05 -0700

    add tree solutions

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,35 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+// solution: like Breadth First Search
+// use a stopper token.
+// added at the end of level
+// otherwise,
+// same method as BFD
+
+function levelWidth(root) {
+  let levels = [0];
+  let arr = [root, "s"];
+
+  // stop when stopper token is only element
+  while (arr.length > 1) {
+    // get first element
+    let currNode = arr.shift();
+
+    // check if done with level
+    if (currNode === "s") {
+      // set up counter for next level
+      // then add back stopper token
+      levels.push(0);
+      arr.push("s");
+    } else {
+      // increment level counter
+      // continue adding children of current level
+      levels[levels.length - 1]++;
+      arr.push(...currNode.children);
+    }
+  }
+  return levels;
+}
 
 module.exports = levelWidth;
--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,52 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+// difference BF vs DF: BF push children to end of array.
+// DF unshift children to start of array
 
-class Tree {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+  // add node to children array
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  //  given data, look at each child of current node
+  // remove any node with data === data
+  remove(data) {
+    this.children = this.children.filter(node => node.data !== data);
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+  // traverse tree Breadth First
+  // invoke fn with each node
+  traverseBF(fn) {
+    let arr = [this.root];
+    while (arr.length) {
+      const currNode = arr.shift();
+      arr.push(...currNode.children);
+      fn(currNode);
+    }
+    return;
+  }
+
+  traverseDF(fn) {
+    let arr = [this.root];
+    while (arr.length) {
+      let currNode = arr.shift();
+      //  add children to start of array
+      arr.unshift(...currNode.children);
+      fn(currNode);
+    }
+    return;
+  }
+}
 
 module.exports = { Tree, Node };
