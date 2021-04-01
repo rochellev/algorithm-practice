685cd0add8b166011ca3cd2e6dcba4ba9b68bf05
rochellev <rochellevroberts@utexas.edu>
2021-04-01 16:45:47 -0700

    add bubble and selection sort algorithms

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,20 +1,48 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
+// O(n^2)
+// find largest, move to right
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
 }
 
+// O(n^2) -- "prove me wrong algorithm"
+// take current index, set as indexOfMin
+// loop to prove wrong, comparing
+// make swap if i != indexOfMin
 function selectionSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
 }
 
+// recursive
 function mergeSort(arr) {
-
+  return arr;
 }
 
-function merge(left, right) {
-
-}
+// iterative
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
