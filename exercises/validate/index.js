556626b47c559f07e3f5ae0cef35268e3c83eb95
rochellev <rochellevroberts@utexas.edu>
2021-04-01 12:28:54 -0700

    add validate function for binary search trees

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,27 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+// need to consider entire tree.
+//
+function validate(node, min = null, max = null) {
+  // check child is less than node
+  // skipped for root node
+  if (max != null && node.data > max) {
+    return false;
+  }
+  if (min != min && node.data < min) {
+    return false;
+  }
+
+  // update max when moving to left
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+  // update min when moving to right
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+  return true;
+}
 
 module.exports = validate;
