ad18c761ba0f2154db14791bc63c5e4088c1ab99
rochellev <rochellevroberts@utexas.edu>
2021-04-01 14:52:42 -0700

    write events library

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,33 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  // each key is an event name, string
+  // value is an array of callbacks. either push or create new array
+  on(eventName, callback) {
+    this.events[eventName]
+      ? this.events[eventName].push(callback)
+      : (this.events[eventName] = [callback]);
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach(event => {
+        event();
+      });
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
